a0dd0d581bb1e4957e5d05c04d0c60d26921813e
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2020-03-28 20:10:17 +0300

    Add files via upload

---
+++ HTTP/Server/Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HTTP SERVER\n");
+            SimpleHTTPServer server;
+            string path;
+            //C:\Users\agcl3\OneDrive\Рабочий стол\Новая папка\http.html
+            Console.WriteLine("Enter Path");
+            path = Console.ReadLine();
+            Console.WriteLine("Enter end to exit\n");
+
+            server = new SimpleHTTPServer(path);
+
+            while (Console.ReadLine() != "end")
+                Console.WriteLine("Enter end to exit\n");
+
+            server.Stop();
+        }
+    }
+}
